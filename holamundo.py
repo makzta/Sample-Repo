bbc492620c36d4d9bef1a3f9ff4730fc21dbad46
Max Timmermann <makzta@gmail.com>
2021-07-04 12:40:27 -0400

    Removed some code

--- holamundo.py
+++ holamundo.py
@@ -1,18 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-print("hola mundo")
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
